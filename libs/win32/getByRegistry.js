6e90fff8705e98d4ec50e169936976136ef1c120
SpecmoComptonJames <james.compton@specmo.com>
2021-03-09 12:50:08 -0500

    Add Windows 10 Fix

--- libs/win32/getByRegistry.js
+++ libs/win32/getByRegistry.js
@@ -12,31 +12,62 @@
   [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
   (New-Object System.Drawing.Text.InstalledFontCollection).Families
 */
-module.exports = () => new Promise((resolve, reject) => {
+module.exports = () => new Promise(async (resolve, reject) => {
   const exec = require('child_process').exec
 
-  const parse = (str) => {
-    let fonts = []
-    str.split('\n').map(ln => {
-      ln = ln.trim()
-      if (!ln || !ln.includes('(TrueType)')) return
+  try {
+    const parse = (str) => {
+      let fonts = []
+      str.split('\n').map(ln => {
+        ln = ln.trim()
+        if (!ln || !ln.includes('(TrueType)')) return
 
-      ln = ln.split('(TrueType')
-      if (ln.length !== 2) return
+        ln = ln.split('(TrueType')
+        if (ln.length !== 2) return
 
-      fonts.push(ln[0].trim())
-    })
+        fonts.push(ln[0].trim())
+      })
 
-    return fonts
-  }
-  //let cmd = `powershell -command "chcp 65001;[System.Reflection.Assembly]::LoadWithPartialName('System.Drawing');(New-Object System.Drawing.Text.InstalledFontCollection).Families"`
-  //replace with powershell with a registry call as on some systems power shell not returning.
-  exec("reg query \"HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts\" /s", { maxBuffer: 1024 * 1024 * 10 }, (err, stdout, stderr) => {
-    if (err) {
-      reject(err)
-      return
+      return fonts
+    }
+    //replace with powershell with a registry call as on some systems power shell not returning.
+    const getSystemFonts = function () {
+      return new Promise(function (res, rej) {
+        exec("reg query \"HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts\" /s", {maxBuffer: 1024 * 1024 * 10}, (err, stdout, stderr) => {
+          if (err) {
+            rej(err)
+            return
+          }
+          res(stdout);
+        })
+      })
+    }
+    //get windows 10 user installed fonts.
+    const getUserFonts = function () {
+      return new Promise(function (res, rej) {
+        exec("reg query \"HKCU\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts\" /s", {maxBuffer: 1024 * 1024 * 10}, (err, stdout, stderr) => {
+          if (err) {
+            if (err.message.includes('The system was unable to find the specified registry key or value.')) {
+              //you must be windows 7 or earlier
+              res("");
+              return
+            } else {
+              //don't know what went wrong.
+              rej(err)
+              return;
+            }
+          }
+          res(stdout)
+        })
+      })
     }
 
-    resolve(parse(stdout))
-  })
+    //add check to combine system and user fonts (WIN10)
+    var list1 = await getSystemFonts();
+    var list2 = await getUserFonts();
+    var list = list1 + list2;
+    resolve(parse(list));
+  } catch(ex) {
+    reject(ex)
+  }
 })
